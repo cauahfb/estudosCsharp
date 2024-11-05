3b9c41b814aee48f707ff7fa28e4d2157403a1c9
Cauã Barbosa <cauaferreirabarbosa@hotmail.com>
2024-11-05 18:48:13 -0300

    first commit

---
+++ ProjetoBanco/Projeto/Projeto/ContaBancaria.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    internal class ContaBancaria
+    {
+        private string _numero;
+        private double _saldo;
+        public string Nome;
+
+        public ContaBancaria(string numero, string nome)
+        {
+            Numero = numero; 
+            Nome = nome;
+        } 
+
+        public string Numero
+        {
+            get { return _numero; }
+            set
+            {
+                if (value.Length == 4)
+                {
+                    _numero = value;
+                }
+                else
+                {
+                    Console.WriteLine("Número digitado inválido, tente novamente");
+                    DefinirNumero();
+
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                {  _saldo = value; }
+            }
+        }
+
+        public void DepositarDinheiro(double saldo)
+        {
+            
+            _saldo += saldo;
+        }
+
+        public void SacarDinheiro(double saldo)
+        {
+            if (saldo > 0)
+            {
+                _saldo -= (saldo + 5);
+            }
+        }
+
+        public void DefinirNumero()
+        {
+            while (true)
+            {
+                Console.Write("Entre com o número da conta: ");
+                string NumeroDaConta = Console.ReadLine();
+                if (NumeroDaConta.Length == 4)
+                {
+                    Numero = NumeroDaConta;
+                    break;
+                }
+                Console.WriteLine("Entrada inválida. Tente novamente.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " 
+                + Numero 
+                + ", Titular: " 
+                + Nome + ", Saldo: $ " 
+                + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ ProjetoBanco/Projeto/Projeto/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Seja bem vindo ao Banco Bruno Cesar");
+            Thread.Sleep(2500);
+            Console.WriteLine("Carregando sistema");
+            for (int i = 0; i < 35; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(150);
+            }
+            Thread.Sleep(2500);
+            Console.Clear();
+            Console.WriteLine("Vi que é a primeira vez que você está acessando, vamos criar sua conta.");
+            Thread.Sleep(2500);
+            Console.WriteLine("Lembre-se o número da conta deve conter 4 dígitos.");
+            Console.Write("Entre com o número da conta: ");
+            string NumeroConta = Console.ReadLine();
+            Console.Write("Entre com o titular da conta: ");
+            string NomeTitular = Console.ReadLine();
+
+            ContaBancaria contaBancaria = new ContaBancaria(NumeroConta, NomeTitular);
+
+            Console.Write("Haverá depósito inicial ? (S/N) ");
+            string DepositoInicial = Console.ReadLine();
+
+            while (DepositoInicial != "S" && DepositoInicial != "N")
+                {
+                Console.WriteLine("Entrada Inválida, tente novamente");
+                Console.Write("Haverá depósito inicial ? (S/N) ");
+                DepositoInicial = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            if (DepositoInicial == "S")
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                contaBancaria.Saldo = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(contaBancaria.ToString());
+
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para depósito: ");
+            contaBancaria.DepositarDinheiro(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("* O Banco Bruno Cesar cobra $ 5 por saque. - Clientes VIP são isentos.");
+            Console.Write("Entre com um valor para saque: ");
+            contaBancaria.SacarDinheiro(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(contaBancaria.ToString());
+            Console.WriteLine();
+
+        }
+    }
+}
